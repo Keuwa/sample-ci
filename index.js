507ae68c8030a6e5e82343928f16e0856901b3d1
Keuwa <vincent.durpoix@hotmail.fr>
2016-06-03 15:04:25 +0200

    c

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 var express = require('express');
+
+var api = express();
+
+api.get('/',function (req,res,next) {
+  res.send('HELLO WORLD')
+})
+
+api.get('/contact',function (req,res,next) {
+  res.send('hey')
+})
+
+console.log("API LISTENING");
+api.listen(3000);
+
+module.exports = api;
---
+++ test/api.scpecs.js
@@ -0,0 +1,47 @@
+var request = require('supertest')
+var api = require('../index')
+
+
+
+describe('API',function () {
+  describe('Contact',function () {
+
+    it('GET /contact should return list of contacts', function () {
+      return request(api)
+      .get('/contact')
+      .send()
+      .expect(200);
+    })
+
+    it('GET/contact/:name should return the list of registered contact with that name',function () {
+      return request(api)
+      .get()
+      .expect(200)
+      .expect(function (res,err) {
+        return res.body instanceof Array
+      })
+    })
+
+      it('POST /contacts should create a new contact',function () {
+        return request(api)
+        .post()
+        .send({name:'edward'})
+        .expect(200);
+      })
+
+      it('PUT /contacts/:name/:new update contact with name',function () {
+        return request(api)
+        .put('/contact/foo/bar')
+        .send()
+        .expect(200);
+      })
+
+      it('DELETE /contact/:name should remove all contacts with the name',function () {
+        return request(api)
+        .delete('/contacts/foo')
+        .send()
+        .expect(200)
+      })
+
+  })
+})
